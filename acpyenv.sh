9576c5b40facbd264f3dee008903f59717cb70f6
heabeounMKTO <heabeoun69zin@gmail.com>
2023-08-31 14:14:45 +0700

    yolov7

--- acpyenv.sh
+++ acpyenv.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/bash
 
+echo "Choose an env to activate"
 SELECTENV=$(cat available.txt | gum choose) 
-echo "activating $SELECTENV"
 
+subdircount=$(find ./$SELECTENV -maxdepth 1 -type d | wc -l)
+if [[ "$subdircount" -eq 1 ]]
+then
+    echo "creating env"
+    python3 -m venv ./$SELECTENV
+    source ./$SELECTENV/bin/activate
+    gum spin --show-output --spinner dot  --title  "installing packages" -- pip install -r $SELECTENV.txt  
+else
+    echo "activating $SELECTENV"
+    set -e
+    source ./$SELECTENV/bin/activate
+fi
